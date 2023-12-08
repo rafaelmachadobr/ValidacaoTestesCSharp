56b86dd13fe2bd3e2690b1acda9270b78eb2fdc2
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-12-08 13:53:16 -0300

    feat: add test for list elements multiplication

--- DesafioTests/ValidacoesListaTests.cs
+++ DesafioTests/ValidacoesListaTests.cs
@@ -38,4 +38,15 @@ public class ValidacoesListaTests
 
         Assert.False(resultado);
     }
+
+    [Fact]
+    public void DeveMultiplicarOsElementosDaListaPor2()
+    {
+        var lista = new List<int> { 5, 7, 8, 9 };
+        var resultadoEsperado = new List<int> { 10, 14, 16, 18 };
+
+        var resultado = _validacoes.MultiplicarNumerosLista(lista, 2);
+
+        Assert.Equal(resultadoEsperado, resultado);
+    }
 }
