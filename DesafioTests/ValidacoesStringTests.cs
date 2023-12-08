8bd567fb1209d9dab38cd90c014a8c90bddefa54
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-12-08 14:08:57 -0300

    feat: add ValidacoesStringTests unit tests

---
+++ DesafioTests/ValidacoesStringTests.cs
@@ -0,0 +1,19 @@
+﻿using DesafioConsole.Services;
+
+namespace DesafioTests;
+
+public class ValidacoesStringTests
+{
+    private ValidacoesString _validacoes = new ValidacoesString();
+
+    [Fact]
+    public void DeveRetornar6QuantidadeCaracteresDaPalavraMatrix()
+    {
+        var texto = "Matrix";
+        var resultadoEsperado = 6;
+
+        var resultado = _validacoes.RetornarQuantidadeCaracteres(texto);
+
+        Assert.Equal(resultadoEsperado, resultado);
+    }
+}
