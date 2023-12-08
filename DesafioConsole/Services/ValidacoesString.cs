0a71c1ba7483a9cb5178c71c4b50c7822391991a
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-12-08 13:07:39 -0300

    feat: add string validation services

---
+++ DesafioConsole/Services/ValidacoesString.cs
@@ -0,0 +1,22 @@
+﻿namespace DesafioConsole.Services;
+
+public class ValidacoesString
+{
+    public int RetornarQuantidadeCaracteres(string texto)
+    {
+        var numeroCaracteres = texto.Length;
+        return numeroCaracteres;
+    }
+
+    public bool ContemCaractere(string texto, string textoProcurado)
+    {
+        var contem = texto.Contains(textoProcurado);
+        return contem;
+    }
+
+    public bool TextoTerminaCom(string texto, string textoProcurado)
+    {
+        var termina = texto.EndsWith(textoProcurado);
+        return termina;
+    }
+}
